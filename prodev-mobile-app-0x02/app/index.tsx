d7b5400b3d316586555153af3bd088ac6a9240bd
faithakinbobola <faithakinbobola2@gmail.com>
2025-09-03 17:10:43 +0100

    Implementing Safe Areas, Images, and Touchable Components in React Native

---
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -0,0 +1,103 @@
+import { Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const { height, width } = Dimensions.get("window");
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <ImageBackground
+          source={require("@/assets/images/mobile-landing.png")}
+          style={styles.background}
+          resizeMode="cover"
+        >
+          <View style={styles.companyLogo}>
+            <Image source={require("@/assets/images/Logo.png")} />
+          </View>
+          <View style={styles.textGroup}>
+            <Text style={styles.textLarge}>Find your favorite place here</Text>
+            <Text style={styles.textSmall}>The best prices for over 2 </Text>
+            <Text style={styles.textSmall}>million properties worldwide</Text>
+          </View>
+
+          <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.transparentButton}>
+                <Text style={styles.textSmall}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ alignItems: "center", paddingVertical: 20 }}>
+              <Text style={{ color: "white" }}>Continue to home</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    width: width,
+    height: height,
+  },
+  companyLogo: {
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+    marginBottom: 50,
+  },
+  textGroup: {
+    alignItems: "center",
+  },
+  textLarge: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "800",
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  textSmall: {
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "200",
+  },
+  transparentButton: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    flex: 1,
+  },
+  button: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    backgroundColor: "white",
+    fontSize: 20,
+    flex: 1,
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    gap: 20,
+    paddingHorizontal: 20,
+  },
+  safeArea: {
+    flex: 1,
+  },
+});

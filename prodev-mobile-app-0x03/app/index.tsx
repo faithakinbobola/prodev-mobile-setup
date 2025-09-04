4c1e1fe99580a00b97c640b3f5c0029816e69388
faithakinbobola <faithakinbobola2@gmail.com>
2025-09-04 17:01:41 +0100

    Explore More Core Components

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         </View>
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
-        <Text style={styles.smallText}>Enter your email and password to sign in</Text>
+        <Text style={styles.smallText}>Enter your email and password to sign in.</Text>
 
         <View style={styles.formGroup}>
           <View>
